022559d792ba7c6e627db84c0eb7e07b5d27c952
r4sd <r4sd.star@gmail.com>
2023-04-27 10:33:42 +0900

    Add the Disk designation

--- modules/aws-ec2-instance/main.tf
+++ modules/aws-ec2-instance/main.tf
@@ -4,9 +4,9 @@ resource "aws_instance" "this" {
   key_name      = var.key_name
   subnet_id     = var.subnet_id
 
-  vpc_security_group_ids = var.security_group_ids
-
-  iam_instance_profile = var.ssm_instance_profile
+  vpc_security_group_ids      = var.security_group_ids
+  iam_instance_profile        = var.ssm_instance_profile
+  associate_public_ip_address = var.associate_public_ip_address
 
   tags = merge(var.common_tags, {
     Name = var.instance_name
@@ -14,5 +14,6 @@ resource "aws_instance" "this" {
 
   root_block_device {
     volume_size = var.root_volume_size
+    volume_type = var.root_volume_type
   }
 }
